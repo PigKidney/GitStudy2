eee0e688e5aeab9c4e2869f365668b5c900eebd2
Gi7E-36 <Gi7E-36@DESKTOP-1NV2HPB>
2023-11-13 12:25:10 +0900

    banana geter seter add

--- GitStudy2/src/fruits/Banana.java
+++ GitStudy2/src/fruits/Banana.java
@@ -4,5 +4,19 @@ public class Banana {
 	int price;
 	String brand;
 	
+	public String getBrand() {
+		return brand;
+	}
 	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
