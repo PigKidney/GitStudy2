79c7c0f982a8e97f20665081f00a0b5cfcc6b00a
Gi7E-36 <Gi7E-36@DESKTOP-1NV2HPB>
2023-11-13 12:34:10 +0900

    master apple add creater

--- GitStudy2/src/fruits/Apple.java
+++ GitStudy2/src/fruits/Apple.java
@@ -1,5 +1,17 @@
 package fruits;
 
 public class Apple {
-
+	int price;
+	int qyt;
+	
+	public int getPrice() {
+		return price;
+	}
+	public int getQyt() {
+		return qyt;
+	}
+	
+	public Apple() {
+		// TODO Auto-generated constructor stub
+	}
 }
